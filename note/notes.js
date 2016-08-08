30e18862c650c60bbd6197f5bcdc805f1cd4d8bc
Andrew Chmil <chilander1@gmail.com>
2016-08-08 22:57:21 +0300

    vf

--- note/notes.js
+++ note/notes.js
@@ -9,13 +9,37 @@ var Note = React.createClass({
         );
     }
 });
+var color1;
 var NoteColor = React.createClass({
-    render: function () {
+    getInitialState: function () {
+        return {
+            selectedOption: '#FF8A80'
+        };
+    },
+    handleOptionChange: function (changeEvent) {
+        this.setState({
+            selectedOption: changeEvent.target.value,
+        });
+        color1 = changeEvent.target.value;
+    },
+
+    render: function (i, props) {
         return (
             <div className="colorB">
-                <input type="radio" name="note-color"/>
-                <input type="radio" name="note-color"/>
-                <input type="radio" name="note-color"/>
+                <input type="radio" value="#FF8A80"
+                       checked={this.state.selectedOption === '#FF8A80'} onChange={this.handleOptionChange}/>
+                <input type="radio" value="#FFD180"
+                       checked={this.state.selectedOption === '#FFD180'} onChange={this.handleOptionChange}/>
+                <input type="radio" value="#FFFF8D"
+                       checked={this.state.selectedOption === '#FFFF8D'} onChange={this.handleOptionChange}/>
+                <input type="radio" value="#CFD8DC"
+                       checked={this.state.selectedOption === '#CFD8DC'} onChange={this.handleOptionChange}/>
+                <input type="radio" value="#80D8FF"
+                       checked={this.state.selectedOption === '#80D8FF'} onChange={this.handleOptionChange}/>
+                <input type="radio" value="#A7FFEB"
+                       checked={this.state.selectedOption === '#A7FFEB'} onChange={this.handleOptionChange}/>
+                <input type="radio" value="#CCFF90"
+                       checked={this.state.selectedOption === '#CCFF90'} onChange={this.handleOptionChange}/>
             </div>
         )
     }
@@ -34,9 +58,11 @@ var NoteEditor = React.createClass({
     handleNoteAdd: function () {
         var newNote = {
             text: this.state.text,
-            color: 'yellow',
+            color: color1,
             id: Date.now()
         };
+        console.log(NoteColor.this)
+
 
         this.props.onNoteAdd(newNote);
         this.setState({text: ''});
@@ -59,11 +85,9 @@ var NoteEditor = React.createClass({
     }
 });
 
-
 var NotesGrid = React.createClass({
     componentDidMount: function () {
         var grid = this.refs.grid;
-        console.log(grid)
         this.msnry = new Masonry(grid, {
             itemSelector: '.note',
             columnWidth: 200,
@@ -139,6 +163,7 @@ var NotesApp = React.createClass({
                 <h2 className="app-header">NotesApp</h2>
                 <NoteEditor onNoteAdd={this.handleNoteAdd}/>
                 <NotesGrid notes={this.state.notes} onNoteDelete={this.handleNoteDelete}/>
+
             </div>
         );
     },
